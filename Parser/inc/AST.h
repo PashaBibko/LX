b5ae34ed749159a3d6c4296f96bd1cec6339b5c3
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-02-21 15:04:23 +0000

    Added AST contents

--- Parser/inc/AST.h
+++ Parser/inc/AST.h
@@ -5,7 +5,7 @@
 #include <type_traits>
 #include <memory>
 
-namespace LX
+namespace LX::FuncAST
 {
 	// Type to track the type of node to stop memory access violations
 	enum class NodeType : short
@@ -46,168 +46,16 @@ namespace LX
 			virtual ~Node_T() {}
 	};
 
-	class Identifier : public Node_T
-	{
-		public:
-			Identifier() : Node_T(NodeType::IDENTIFIER) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::IDENTIFIER;
-			}
-	};
-
-	class Declaration : public Node_T
-	{
-		public:
-			Declaration() : Node_T(NodeType::VAR_DEC) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::VAR_DEC;
-			}
-	};
-
-	class Assignment : public Node_T
-	{
-		public:
-			Assignment() : Node_T(NodeType::VAR_ASSIGN) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::VAR_ASSIGN;
-			}
-	};
-
-	class Operation : public Node_T
-	{
-		public:
-			Operation() : Node_T(NodeType::OPERATION) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::OPERATION;
-			}
-	};
-
-	class UnaryOperation : public Node_T
-	{
-		public:
-			UnaryOperation() : Node_T(NodeType::UNARY_OPERATION) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::UNARY_OPERATION;
-			}
-	};
-
-	class BracketExpression : public Node_T
-	{
-		public:
-			BracketExpression() : Node_T(NodeType::BRACKET_EXPR) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::BRACKET_EXPR;
-			}
-	};
-
-	class FunctionCall : public Node_T
-	{
-		public:
-			FunctionCall() : Node_T(NodeType::FUNC_CALL) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::FUNC_CALL;
-			}
-	};
-
-	class StringLiteral : public Node_T
-	{
-		public:
-			StringLiteral() : Node_T(NodeType::STRING_LITERAL) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::STRING_LITERAL;
-			}
-	};
-
-	class NumberLiteral : public Node_T
-	{
-		public:
-			NumberLiteral() : Node_T(NodeType::NUMBER_LITERAL) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::NUMBER_LITERAL;
-			}
-	};
-
-	class IfStatement : public Node_T
-	{
-		public:
-			IfStatement() : Node_T(NodeType::IF_STATEMENT) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::IF_STATEMENT;
-			}
-	};
-
-	class WhileLoop : public Node_T
-	{
-		public:
-			WhileLoop() : Node_T(NodeType::WHILE_LOOP) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::WHILE_LOOP;
-			}
-	};
-
-	class ForLoop : public Node_T
-	{
-		public:
-			ForLoop() : Node_T(NodeType::FOR_LOOP) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::FOR_LOOP;
-			}
-	};
-
-	class ReturnStatement : public Node_T
-	{
-		public:
-			ReturnStatement() : Node_T(NodeType::RETURN_STATEMENT) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::RETURN_STATEMENT;
-			}
-	};
-
-	class GoToStatement : public Node_T
-	{
-		public:
-			GoToStatement() : Node_T(NodeType::GOTO_STATEMENT) {}
-
-			static constexpr NodeType EnumVal()
-			{
-				return NodeType::GOTO_STATEMENT;
-			}
-	};
-
-	class NodeAccseser
+	// Safe way to access the nodes
+	class Node
 	{
 		private:
 			Node_T* ptr;
 
 		public:
-			explicit NodeAccseser(Node_T* _ptr)
-				: ptr(_ptr) {}
+			explicit Node(Node_T* _ptr = nullptr)
+				: ptr(_ptr) {
+			}
 
 			template<typename T>
 			inline T* as()
@@ -216,5 +64,302 @@ namespace LX
 
 				return (T::EnumVal() == ptr->Type()) ? (T*)ptr : nullptr;
 			}
+	};
+
+	// -- All AST Nodes -- //
+	// Constructors will be moved to a .cpp file "later"
+
+	class Identifier : public Node_T
+	{
+		public:
+			// Constructor
+			Identifier(const std::string& name) :
+				m_Name(name), Node_T(NodeType::IDENTIFIER)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::IDENTIFIER;
+			}
+
+			// Name of the identifier
+			const std::string m_Name;
+
+			// Will eventually hold information like namespace (parent)
+	};
+
+	class Declaration : public Node_T
+	{
+		public:
+			// Constructor
+			Declaration(Identifier name , Identifier type) :
+				m_Name(name), m_Type(type), Node_T(NodeType::VAR_DEC)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::VAR_DEC;
+			}
+
+			// Type of the variable
+			Identifier m_Type; // Will eventually need to hold more info such as ref or ptr
+
+			// Name of the variable
+			Identifier m_Name;
+	};
+
+	class Assignment : public Node_T
+	{
+		public:
+			// Constructor
+			Assignment(Identifier asignee, Node value) :
+				m_Asignee(asignee), m_Value(value), Node_T(NodeType::VAR_ASSIGN)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::VAR_ASSIGN;
+			}
+
+			// The variable that is being assigned to
+			Identifier m_Asignee;
+
+			// Value that it is being set to
+			Node m_Value;
+	};
+
+	class Operation : public Node_T
+	{
+		public:
+			// Constructor
+			Operation(Node lhs, TokenTypes::Func operand, Node rhs) :
+				m_Lhs(lhs), m_Operand(operand), m_Rhs(rhs), Node_T(NodeType::OPERATION)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::OPERATION;
+			}
+
+			// Sides of the operation
+			Node m_Lhs, m_Rhs;
+
+			// Operation to apply to the sides
+			TokenTypes::Func m_Operand;
+	};
+
+	class UnaryOperation : public Node_T
+	{
+		public:
+			// Fancy boolean for which side the operation is
+			enum class Side : bool
+			{
+				LEFT = false,
+				RIGHT = true
+			};
+
+			// Constructor
+			UnaryOperation(Node value, TokenTypes::Func operand, Side side = Side::LEFT)
+				: m_Value(value), m_Operand(operand), m_Side(side), Node_T(NodeType::UNARY_OPERATION)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::UNARY_OPERATION;
+			}
+
+			// Operand
+			TokenTypes::Func m_Operand;
+
+			// Which side the operand is on
+			Side m_Side;
+
+			// What to apply the operand to
+			Node m_Value;
+	};
+
+	class BracketExpression : public Node_T
+	{
+		public:
+			// Constructor
+			BracketExpression(Node expr) :
+				m_Expression(expr), Node_T(NodeType::BRACKET_EXPR)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::BRACKET_EXPR;
+			}
+
+			// Expression within the brackets
+			Node m_Expression;
+	};
+
+	class FunctionCall : public Node_T
+	{
+		public:
+			// Constructor
+			FunctionCall(Identifier name, std::vector<Node> arguments = std::vector<Node>())
+				: m_Name(name), m_Arguments(arguments), Node_T(NodeType::FUNC_CALL)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::FUNC_CALL;
+			}
+
+			// Name of the function
+			Identifier m_Name;
+
+			// Arguments
+			std::vector<Node> m_Arguments;
+	};
+
+	class StringLiteral : public Node_T
+	{
+		public:
+			// Constructor
+			StringLiteral(const std::string& value) :
+				m_Value(value), Node_T(NodeType::STRING_LITERAL)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::STRING_LITERAL;
+			}
+
+			// Contents of the string literal
+			const std::string m_Value;
+	};
+
+	class NumberLiteral : public Node_T
+	{
+		public:
+			// Constructor
+			NumberLiteral(const std::string& value) :
+				m_Value(value), Node_T(NodeType::NUMBER_LITERAL)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::NUMBER_LITERAL;
+			}
+
+			// The number (yes it is stored as a string (yes its cursed))
+			const std::string m_Value;
+	};
+
+	class IfStatement : public Node_T
+	{
+		public:
+			// Constructor
+			IfStatement(TokenTypes::Func type, Node condition, std::unique_ptr<IfStatement> chain) :
+				m_Type(type), m_Condition(condition), m_Chain(std::move(chain)), Node_T(NodeType::IF_STATEMENT)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::IF_STATEMENT;
+			}
+
+			// Stores if it is { IF, ELIF, ELSE }
+			TokenTypes::Func m_Type;
+
+			// Condition. If m_Type == ELSE this is null
+			Node m_Condition;
+
+			// Body
+			// (unknown) m_Body; // Dont know how this will work yet
+
+			// Next in the chain (if any)
+			std::unique_ptr<IfStatement> m_Chain;
+	};
+
+	class WhileLoop : public Node_T
+	{
+		public:
+			// Constructor
+			WhileLoop(Node condition) :
+				m_Condition(condition), Node_T(NodeType::WHILE_LOOP)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::WHILE_LOOP;
+			}
+
+			// Condition of the loop
+			Node m_Condition;
+
+			// Body
+			// (unknown) m_Body; // Dont know how this will work yet
+	};
+
+	class ForLoop : public Node_T
+	{
+		public:
+			// Constructor
+			ForLoop(Node condition) :
+				m_Condition(condition), Node_T(NodeType::FOR_LOOP)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::FOR_LOOP;
+			}
+
+			// Condition of the loop
+			Node m_Condition;
+
+			// Body
+			// (unknown) m_Body; // Dont know how this will work yet
+	};
+
+	class ReturnStatement : public Node_T
+	{
+		public:
+			// Constructor
+			ReturnStatement(Node value) :
+				m_Value(value), Node_T(NodeType::RETURN_STATEMENT)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::RETURN_STATEMENT;
+			}
+
+			// Value that it can return
+			Node m_Value;
+	};
+
+	class GoToStatement : public Node_T
+	{
+		public:
+			// Constructor
+			GoToStatement(TokenTypes::Func type) :
+				m_GoToType(type), Node_T(NodeType::GOTO_STATEMENT)
+			{}
+
+			// Getter for polymorphism
+			static constexpr NodeType EnumVal()
+			{
+				return NodeType::GOTO_STATEMENT;
+			}
+			
+			// What type of go-to is it
+			TokenTypes::Func m_GoToType;
 	};
 }
