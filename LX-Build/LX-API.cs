3386272f2a6a7f99459818a2414effc98e863870
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-05-03 17:11:09 +0100

    Used modern approach for C#/C++ interop

--- LX-Build/LX-API.cs
+++ LX-Build/LX-API.cs
@@ -3,15 +3,17 @@ using System.Runtime.InteropServices;
 
 namespace LX_Build
 {
-    internal class LX_API
+    internal partial class LX_API
     {
         // Imports SetDllDirectory to change where Dlls are imported from //
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern bool SetDllDirectory(string lpPathName);
+        [LibraryImport("kernel32.dll", EntryPoint = "SetDllDirectoryW", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool SetDllDirectory(string lpPathName);
 
         // Imports the Frontend of the compiler //
-        [DllImport("Generator.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
-        public static extern int GenIR(string arg1, string arg2, string? arg3);
+        [LibraryImport("Generator.dll", StringMarshalling = StringMarshalling.Custom, StringMarshallingCustomType = typeof(System.Runtime.InteropServices.Marshalling.AnsiStringMarshaller))]
+        [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
+        public static partial int GenIR(string arg1, string arg2, string? arg3);
 
         // Sets the directory to import the DLLs from //
         public static void Init()
